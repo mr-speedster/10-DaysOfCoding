120eae70427980d97cb3f50c7b62e30319990b55
Ajith <ajithd78564@gmail.com>
2021-11-15 17:55:04 +0530

    classs and object bascic program

---
+++ firstClssProgram.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(this,name,age,job):
+        this.name=name
+        this.age=age
+        this.job=job
+        print(this.name,this.age,this.job)
+Person=Employee("Ajith",5,"Web Dev")
