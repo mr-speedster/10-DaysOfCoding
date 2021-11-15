06e1e2b61a6355dca3b044aac75b28f2da8c113d
Ajith <ajithd78564@gmail.com>
2021-11-15 15:20:51 +0530

    list

---
+++ list.py
@@ -0,0 +1,28 @@
+list1=[1,2,3,4,5,6,7,8,9,10]
+print(list1)
+print(len(list1))
+print(list1[2])
+print(list1[2])
+
+for i in list1:
+    print(i)
+
+#listilott addakkan vendi append use cheyyam(ith listinte last aan add akunne)
+
+list1.append("Ajith")
+
+for j in list1:
+    print(j)
+
+#specify cheyyth insert cheyyan vendi nammal insert use cheyyum athin index,value rand params ond
+
+list1.insert(1,"Achu")
+
+for k in list1:
+    print(k)
+
+list1.clear() #clear cheyyan vendi
+
+del list1 #delete cheyyan vendi
+
+print(list1) #error becouse of the list is deleted
